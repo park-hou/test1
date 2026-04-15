c3aa7761c66ee35cbcb912aa9ced1784708577aa
park-hou <himedi@knou.ac.kr>
2026-04-15 19:26:38 +0900

    tprma alc tlftnfuddor rlsmd cnrk

--- salary.py
+++ salary.py
@@ -4,3 +4,8 @@ rate = float(input("시급: "))
 salary = hours * rate
 print("이름:", name)
 print("급여:", salary)
+# 세금 10% 계산
+tax = salary * 0.1
+net_salary = salary - tax
+print("세금:", tax)
+print("실수령액:", net_salary)
