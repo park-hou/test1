b0bc91b00644590508486cd39efa996139a986f1
park-hou <himedi@knou.ac.kr>
2026-04-15 20:01:28 +0900

    세금 15%

--- salary.py
+++ salary.py
@@ -4,8 +4,8 @@ rate = float(input("시급: "))
 salary = hours * rate
 print("이름:", name)
 print("급여:", salary)
-# 세금 10% 계산
-tax = salary * 0.1
+# 세금 15% 계산
+tax = salary * 0.15
 net_salary = salary - tax
 print("세금:", tax)
 print("실수령액:", net_salary)
